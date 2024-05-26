5cc7f6b4113e006a763d1cbf8259fea4bc9484bf
Erick20247 <Torot0086@gmail.vom>
2024-05-26 02:03:28 -0400

    cree la migrations

--- registrotecnicos/registrotecnicos/Migrations/20240526032840_inicial.cs
+++ registrotecnicos/registrotecnicos/Migrations/20240526032840_inicial.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore.Migrations;
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
 
 #nullable disable
 
@@ -10,6 +11,23 @@ namespace registrotecnicos.Migrations
         /// <inheritdoc />
         protected override void Up(MigrationBuilder migrationBuilder)
         {
+            migrationBuilder.CreateTable(
+                name: "incentivosTecnicos",
+                columns: table => new
+                {
+                    IncentivoId = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Fecha = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    TecnicoId = table.Column<int>(type: "INTEGER", nullable: false),
+                    Descripcion = table.Column<string>(type: "TEXT", nullable: false),
+                    CantidadServicios = table.Column<int>(type: "INTEGER", nullable: false),
+                    Monto = table.Column<decimal>(type: "TEXT", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_incentivosTecnicos", x => x.IncentivoId);
+                });
+
             migrationBuilder.CreateTable(
                 name: "Tecnicos",
                 columns: table => new
@@ -28,6 +46,9 @@ namespace registrotecnicos.Migrations
         /// <inheritdoc />
         protected override void Down(MigrationBuilder migrationBuilder)
         {
+            migrationBuilder.DropTable(
+                name: "incentivosTecnicos");
+
             migrationBuilder.DropTable(
                 name: "Tecnicos");
         }
