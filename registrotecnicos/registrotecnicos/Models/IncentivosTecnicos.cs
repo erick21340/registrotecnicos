5760208aa8817bbad1bc1aaa576aa44dfd2eeec6
Erick20247 <Torot0086@gmail.vom>
2024-05-26 02:04:06 -0400

    añadi el models

---
+++ registrotecnicos/registrotecnicos/Models/IncentivosTecnicos.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace registrotecnicos.Models
+{
+    public class Incentivos
+    {
+        [Key]
+        public int  IncentivoId { get; set; }
+
+
+        public DateTime Fecha { get; set; }
+
+
+        public int TecnicoId {  get; set; }
+
+        public string Descripcion { get; set; }
+
+        public int CantidadServicios { get; set; }
+
+
+        public decimal? Monto { get; set; }
+    }
+}
